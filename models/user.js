2e41144ed6634a25c917f24f98386b811c22eebb
Anton Virtanen <avirtanen@hotmail.fr>
2017-02-20 16:55:10 +0100

    wat

--- models/user.js
+++ models/user.js
@@ -1,12 +1,13 @@
 const mongoose     = require('mongoose');
 const Schema       = mongoose.Schema;
+const Offer        = require('../models/offer');
 
 const UserSchema = new Schema({
 
       username  : String,
       password  : String,
       email     : String,
-      offers    : []
+      offers    : [{ type: Schema.Types.ObjectId, ref: 'Offer' }]
 
 });
 
--- routes/offers.js
+++ routes/offers.js
@@ -2,28 +2,42 @@ var express = require('express');
 var router  = express.Router();
 const User  = require("../models/user");
 const Offer = require("../models/offer");
+const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
 /*GET offer page */
-router.get("/offer",(req, res) => {
+router.get("/offer", ensureLoggedIn(), (req, res) => {
   res.render("offer", { user: req.user });
 });
 
 /* POST offer page */
 router.post('/offer', (req, res, next) => {
   const offerInfo = {
+      _supplier    : req.user._id,
       name         : req.body.name,
-      // offer        : req.body.offer,
       offer        : Array.isArray(req.body.offer) ? req.body.offer : [req.body.offer],
       description  : req.body.description
   };
-
   const newOffer = new Offer(offerInfo);
-console.log(newOffer._id);
+
+
+
 
   newOffer.save( (err) => {
     if (err) { return next(err) }
-    return res.redirect('/main');
+
+    Offer
+   .findOne({ name: newOffer.name })
+   .populate('_supplier')
+   .exec(function (err, offer) {
+     if (err) {return handleError(err)};
+     console.log('The creator is %s', offer._supplier.username);
+     // prints "The creator is Aaron"
+   });
+
+   return res.redirect('/main');
+
   });
+
 });
 
 module.exports = router;
