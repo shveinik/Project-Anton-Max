fdabe78a3af0ae47a0d74c5e3d7b96d4f329e0b4
Max Smotrov <max.smotrov@yahoo.com>
2017-02-24 16:37:34 +0100

    max 888

--- public/javascripts/info.js
+++ public/javascripts/info.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $("#infoFooter").hide();
   $("#info").click(function(){
-    $("#map").toggle(500);
-    $("#infoFooter").toggle(500);
+    $("#map").toggle(400);
+    $("#infoFooter").toggle(400);
     $("#infoFooter").css("background-image","url(https://s-media-cache-ak0.pinimg.com/originals/98/ed/c3/98edc37f1c0bb19584cd76e71e198aab.jpg)");
 });
 
